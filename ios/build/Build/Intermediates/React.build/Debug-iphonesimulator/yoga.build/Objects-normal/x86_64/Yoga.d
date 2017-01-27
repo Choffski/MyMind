dd100455b9c12eaaf5c526ba5c554b3939412f86
Samuli <samuli.laine@oneup.fi>
2017-01-27 15:28:40 +0200

    .gitignore

--- ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
+++ ios/build/Build/Intermediates/React.build/Debug-iphonesimulator/yoga.build/Objects-normal/x86_64/Yoga.d
@@ -1,6 +1,2 @@
 dependencies: \
-  /Users/Developer/Desktop/Applications/MyMind/MyMind/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c \
-  /Users/Developer/Desktop/Applications/MyMind/MyMind/node_modules/react-native/ReactCommon/yoga/yoga/YGNodeList.h \
-  /Users/Developer/Desktop/Applications/MyMind/MyMind/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h \
-  /Users/Developer/Desktop/Applications/MyMind/MyMind/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.h \
-  /Users/Developer/Desktop/Applications/MyMind/MyMind/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h
+  /Users/Developer/Desktop/Applications/MyMind2/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.c
